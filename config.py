170a56321874f6dd012b4f29ff7a269588e07541
nornorno <noureteo@gmail.com>
2024-06-10 03:56:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Fox455664/Fox11.git",
+    "https://github.com/nornorno/HGR",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
